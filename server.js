92179f05fdf7880d6431f3a95c7b78c18d4f80ac
Denys Shpak <mr.shpak@gmail.com>
2024-03-06 17:55:06 +0200

    cors added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Joi = require("joi");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
