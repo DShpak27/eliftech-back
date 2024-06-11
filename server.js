5a4881ff910a8cc2951b0696308c729e3c372d9b
Denys Shpak <mr.shpak@gmail.com>
2024-06-11 18:15:59 +0300

    perf: changes for vercel deploy

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const { ordersRouter } = require("./src/routes/orders-router.js");
-const { pharmsRouter } = require("./src/routes/pharms-router.js");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { ordersRouter } = require('./src/routes/orders-router.js');
+const { pharmsRouter } = require('./src/routes/pharms-router.js');
 
 const app = express();
 app.use(cors());
@@ -12,8 +12,10 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.CONNECTION_STRING);
 
-app.use("/", [ordersRouter, pharmsRouter]);
+app.use('/', [ordersRouter, pharmsRouter]);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
